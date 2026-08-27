32d73d9f34bf0f184cb1b840cefa69215bfc3e0b
joacofg <glamblox@gmail.com>
2026-08-27 14:50:17 -0300

    Refactor player handling to use sprite groups for updates and drawing

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        player.update(0.016)
-        player.draw(screen)
+        updatable.update(0.016)
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
         pygame.display.flip()
 
 
